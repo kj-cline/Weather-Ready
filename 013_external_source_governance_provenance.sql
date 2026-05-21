0fb07a612f035439be9b1edcfc171af06a99a3bb
KJ Cline <155862464+kj-cline@users.noreply.github.com>
2026-05-20 22:15:17 -0400

    Delete 013_external_source_governance_provenance.sql

--- 013_external_source_governance_provenance.sql
+++
@@ -1,3 +0,0 @@
-ALTER TABLE external_source_catalog ADD COLUMN IF NOT EXISTS governance_source TEXT;
-ALTER TABLE external_source_catalog ADD COLUMN IF NOT EXISTS governance_provider TEXT;
-ALTER TABLE external_source_catalog ADD COLUMN IF NOT EXISTS governance_fallback_reason TEXT;
