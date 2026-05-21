e549d96dd8db76db41ef131d706b57991518b1db
KJ Cline <155862464+kj-cline@users.noreply.github.com>
2026-05-20 22:15:55 -0400

    Delete 017_location_context_hints.sql

--- 017_location_context_hints.sql
+++
@@ -1,5 +0,0 @@
-ALTER TABLE location_context_profile
-ADD COLUMN IF NOT EXISTS weather_sensitivity_hint DOUBLE;
-
-ALTER TABLE location_context_profile
-ADD COLUMN IF NOT EXISTS demand_volatility_hint DOUBLE;
