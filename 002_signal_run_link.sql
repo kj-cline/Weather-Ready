c82f011ac3043002e05b37718f616914f1627079
KJ Cline <155862464+kj-cline@users.noreply.github.com>
2026-05-20 22:12:27 -0400

    Delete 002_signal_run_link.sql

--- 002_signal_run_link.sql
+++
@@ -1,5 +0,0 @@
-ALTER TABLE external_signal_log
-ADD COLUMN IF NOT EXISTS source_prediction_run_id TEXT;
-
-CREATE INDEX IF NOT EXISTS idx_external_signal_operator_run
-    ON external_signal_log(operator_id, source_prediction_run_id);
